dc9773a9630e50ef37d370681d85a78997673cc0
apeksha047 <apeksha5589pavasiya@gmail.com>
2020-10-25 22:45:51 +0530

    permission controller--1.0

--- app/src/main/java/com/example/secureaty/MainActivity.java
+++ app/src/main/java/com/example/secureaty/MainActivity.java
@@ -9,17 +9,26 @@ import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
-    Button Scanbtn;
+    Button Scanbtn, pController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Scanbtn = (Button) findViewById(R.id.Scanbutton);
+        Scanbtn =  findViewById(R.id.Scanbutton);
+        pController = findViewById(R.id.permission_controller_btn);
+
         Scanbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ChooseFileActivity();
+            }
+        });
+
+        pController.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pControllerActivity();
             }
         });
     }
@@ -28,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
     public void ChooseFileActivity(){
         Intent intent_chooseFileActivity= new Intent(this,ChooseFileActivity.class);
         startActivity(intent_chooseFileActivity);
+    }
+
+    public void pControllerActivity(){
+        Intent intent_pControllerActivity= new Intent(this,pControllerActivity.class);
+        startActivity(intent_pControllerActivity);
     }
 }
 
