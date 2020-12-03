5f6908366230833be8c2495545e44548dcfbb2fe
Marynk <36852627+Marynk@users.noreply.github.com>
2020-12-03 16:23:17 +0200

    Permission checker rebased (#12)
    * permission checker done (Mehak)
    
    * permission checker
    
    * Post-merge fixes
    
    Co-authored-by: mehakashik <mehakashik@gmail.com>

--- app/src/main/java/com/example/secureaty/MainActivity.java
+++ app/src/main/java/com/example/secureaty/MainActivity.java
@@ -4,43 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
-
 
 public class MainActivity extends AppCompatActivity {
-    Button Scanbtn;
+    Button Scanbtn, permissionControllerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        try {
-//            Scanner scanner = new Scanner(new File("/data/user/0/com.example.secureaty/cache/cloak.txt"));
-//            StringBuilder result = new StringBuilder();
-//            result.append(scanner.useDelimiter("\0").next());
-//            scanner.close();
-//            boolean OverlayDetected = OverlayDetector.detect(result.toString());
-//            Log.d("overlaydetected", String.valueOf(OverlayDetected));
-//
-//        } catch (IOException e){}
-        Scanbtn = (Button) findViewById(R.id.Scanbutton);
+        Scanbtn = findViewById(R.id.Scanbutton);
+        permissionControllerBtn = (Button) findViewById(R.id.permissionControllerBtn);
+
         Scanbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ChooseFileActivity();
             }
         });
+
+        permissionControllerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAllActivityInstalledIntent();
+            }
+        });
     }
 
+    private void openAllActivityInstalledIntent(){
+        Intent allInstalledActivityIntent = new Intent(MainActivity.this, AllInstalledAppsActivity.class);
+        startActivity(allInstalledActivityIntent);
+    }
 
-    public void ChooseFileActivity(){
-        Intent intent_chooseFileActivity= new Intent(this,ChooseFileActivity.class);
+    public void ChooseFileActivity() {
+//        Intent intent_chooseFileActivity= new Intent(this,ChooseFileActivity.class);
+//        startActivity(intent_chooseFileActivity);
+        Intent intent_chooseFileActivity = new Intent(this, ChooseFileActivity.class);
+        intent_chooseFileActivity.putExtra("EXTRA_SESSION_ID", "0");
+        startActivity(intent_chooseFileActivity);
+    }
+
+    public void check(View view) {
+        Intent intent_chooseFileActivity = new Intent(this, ChooseFileActivity.class);
+        intent_chooseFileActivity.putExtra("EXTRA_SESSION_ID", "1");
         startActivity(intent_chooseFileActivity);
     }
 }
