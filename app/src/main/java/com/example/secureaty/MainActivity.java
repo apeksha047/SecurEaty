bfb4e6a3564b5aab4703c3b9998df5c649b8cf69
Maryna Kluban <mkluban@gmail.com>
2020-12-03 16:16:47 +0200

    Post-merge fixes

--- app/src/main/java/com/example/secureaty/MainActivity.java
+++ app/src/main/java/com/example/secureaty/MainActivity.java
@@ -4,13 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
 
 
 public class MainActivity extends AppCompatActivity {
